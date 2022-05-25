49aa33296c0d88eb892543577f7af4a36da2f3f3
arnaud.notter <arnaud.notter@epita.fr>
2022-05-25 13:32:18 +0200

    Add feater to add mail inside db

--- cocktail-web-client/src/components/App.tsx
+++ cocktail-web-client/src/components/App.tsx
@@ -5,6 +5,7 @@ import '../styles/container.css';
 import CocktailList from './CocktailList';
 import SearchBar from './SearchBar';
 import DetailledCocktail from './DetailledCocktail';
+import FooterCocktail from './FooterCocktail';
 
 function App() {
   const[WhichCocktail, UpdateCocktail] = useState<string>('');
@@ -19,6 +20,7 @@ function App() {
         <CocktailList WhichCocktail={WhichCocktail} UpdateCocktail={UpdateCocktail}></CocktailList> 
         : <DetailledCocktail WhichCocktail={WhichCocktail} UpdateCocktail={UpdateCocktail}></DetailledCocktail>
       }
+      <FooterCocktail></FooterCocktail>
     </div>
   );
 }
---
+++ cocktail-web-client/src/components/FooterCocktail.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "../styles/FooterCocktail.css"
+
+function FooterCocktail(){
+    const [mail, setMail] = useState<string>('');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMail(event.target.value);
+        console.log(mail);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("the mail: " + mail);
+    }
+
+
+
+    return (
+        <div className="container_footer">
+            <div className="Text">Pour les passionnés de spam</div>
+            <form className ="InputArea" onSubmit={handleSubmit}>
+                <input className="areamail"  type="text" value={mail} onChange={handleChange}></input>
+                <button className="addmail" type="submit" >Search</button>
+            </form>
+            </div>
+    )
+}
+
+export default FooterCocktail;
